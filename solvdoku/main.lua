d53c332ed4982dff3a43b23921bb80c3cbca0dfa
marcotrosi <mhtrosi@googlemail.com>
2019-05-04 19:43:13 +0200

    initial commit

---
+++ solvdoku/main.lua
@@ -0,0 +1,311 @@
+--[[
+press q to quit
+press r to toggle reminder flags visibility
+press a to toggle auto-fill mode
+press f to to run auto-fill ones
+press c to clear the board (not implemented yet)
+press h,j,k,l or the arrow keys to move the selection
+press 1-9 to put values in the boxes
+press 0 or spacebar to clear a box
+
+TODO
+   - mouse support
+   - fit reminder flags to box
+   - try to reduce CPU usage
+--]]
+
+local sd = require "solvdoku"
+
+
+function getBoxPosX(i) -- <<<
+   return OffsetX+( ((i-1)%9) * Size)
+end -- >>>
+
+function getBoxPosY(i) -- <<<
+   return OffsetY+( math.floor((i-1)/9) * Size)
+end -- >>>
+
+function updateData(GuiData, GameData) -- <<<
+
+   Width, Height = love.graphics.getDimensions()
+   BoardSize     = math.min(Width, Height) * 0.8
+   OffsetX       = (Width  - BoardSize)/2
+   OffsetY       = (Height - BoardSize)/2
+   Size          = BoardSize/9
+   FontHeight    = math.floor(Size*0.4)
+   Font          = love.graphics.newFont("GeosansLight.ttf", FontHeight)
+
+   XA = OffsetX+(3*Size)
+   XB = OffsetX+(6*Size)
+   XC = OffsetX+(9*Size)
+   YA = OffsetY+(3*Size)
+   YB = OffsetY+(6*Size)
+   YC = OffsetY+(9*Size)
+
+   ReminderFontHeight = math.floor(Size/9)
+   ReminderFont       = love.graphics.newFont("GeosansLight.ttf", ReminderFontHeight)
+
+   for i,v in ipairs(GameData) do
+      GuiData[i].x = getBoxPosX(i)
+      GuiData[i].y = getBoxPosY(i)
+      GuiData[i].nx= getBoxPosX(i) + Size/2 - Font:getWidth('0')/2
+      GuiData[i].ny= getBoxPosY(i) + Size/2 - FontHeight/2
+   end
+
+   collectgarbage()
+end -- >>>
+
+function updateSelected() -- <<<
+   SelectedX = getBoxPosX(Selected)
+   SelectedY = getBoxPosY(Selected)
+end -- >>>
+
+function drawBox(t) -- <<<
+   local g = love.graphics
+
+   g.rectangle("line", t.x, t.y, Size, Size)
+   g.setFont(Font)
+   g.print(t.d.value_s, t.nx, t.ny)
+
+   if ShowReminder then
+      -- TODO fit to box
+      g.setFont(ReminderFont)
+      for i,v in ipairs(t.d) do
+         if v then
+            g.print(tostring(i), t.x+(i*ReminderFontHeight)-1, t.y+Size-ReminderFontHeight-3)
+         end
+      end
+   end
+end -- >>>
+
+
+function love.load() -- <<<
+
+   love.graphics.setLineStyle("smooth")
+   love.keyboard.setKeyRepeat(true)
+
+   Data = -- <<<
+   {
+      { x = 0, y = 0, nx= 0, ny= 0, d=sd.GameData[1] , },
+      { x = 0, y = 0, nx= 0, ny= 0, d=sd.GameData[2] , },
+      { x = 0, y = 0, nx= 0, ny= 0, d=sd.GameData[3] , },
+      { x = 0, y = 0, nx= 0, ny= 0, d=sd.GameData[4] , },
+      { x = 0, y = 0, nx= 0, ny= 0, d=sd.GameData[5] , },
+      { x = 0, y = 0, nx= 0, ny= 0, d=sd.GameData[6] , },
+      { x = 0, y = 0, nx= 0, ny= 0, d=sd.GameData[7] , },
+      { x = 0, y = 0, nx= 0, ny= 0, d=sd.GameData[8] , },
+      { x = 0, y = 0, nx= 0, ny= 0, d=sd.GameData[9] , },
+      { x = 0, y = 0, nx= 0, ny= 0, d=sd.GameData[10], },
+      { x = 0, y = 0, nx= 0, ny= 0, d=sd.GameData[11], },
+      { x = 0, y = 0, nx= 0, ny= 0, d=sd.GameData[12], },
+      { x = 0, y = 0, nx= 0, ny= 0, d=sd.GameData[13], },
+      { x = 0, y = 0, nx= 0, ny= 0, d=sd.GameData[14], },
+      { x = 0, y = 0, nx= 0, ny= 0, d=sd.GameData[15], },
+      { x = 0, y = 0, nx= 0, ny= 0, d=sd.GameData[16], },
+      { x = 0, y = 0, nx= 0, ny= 0, d=sd.GameData[17], },
+      { x = 0, y = 0, nx= 0, ny= 0, d=sd.GameData[18], },
+      { x = 0, y = 0, nx= 0, ny= 0, d=sd.GameData[19], },
+      { x = 0, y = 0, nx= 0, ny= 0, d=sd.GameData[20], },
+      { x = 0, y = 0, nx= 0, ny= 0, d=sd.GameData[21], },
+      { x = 0, y = 0, nx= 0, ny= 0, d=sd.GameData[22], },
+      { x = 0, y = 0, nx= 0, ny= 0, d=sd.GameData[23], },
+      { x = 0, y = 0, nx= 0, ny= 0, d=sd.GameData[24], },
+      { x = 0, y = 0, nx= 0, ny= 0, d=sd.GameData[25], },
+      { x = 0, y = 0, nx= 0, ny= 0, d=sd.GameData[26], },
+      { x = 0, y = 0, nx= 0, ny= 0, d=sd.GameData[27], },
+      { x = 0, y = 0, nx= 0, ny= 0, d=sd.GameData[28], },
+      { x = 0, y = 0, nx= 0, ny= 0, d=sd.GameData[29], },
+      { x = 0, y = 0, nx= 0, ny= 0, d=sd.GameData[30], },
+      { x = 0, y = 0, nx= 0, ny= 0, d=sd.GameData[31], },
+      { x = 0, y = 0, nx= 0, ny= 0, d=sd.GameData[32], },
+      { x = 0, y = 0, nx= 0, ny= 0, d=sd.GameData[33], },
+      { x = 0, y = 0, nx= 0, ny= 0, d=sd.GameData[34], },
+      { x = 0, y = 0, nx= 0, ny= 0, d=sd.GameData[35], },
+      { x = 0, y = 0, nx= 0, ny= 0, d=sd.GameData[36], },
+      { x = 0, y = 0, nx= 0, ny= 0, d=sd.GameData[37], },
+      { x = 0, y = 0, nx= 0, ny= 0, d=sd.GameData[38], },
+      { x = 0, y = 0, nx= 0, ny= 0, d=sd.GameData[39], },
+      { x = 0, y = 0, nx= 0, ny= 0, d=sd.GameData[40], },
+      { x = 0, y = 0, nx= 0, ny= 0, d=sd.GameData[41], },
+      { x = 0, y = 0, nx= 0, ny= 0, d=sd.GameData[42], },
+      { x = 0, y = 0, nx= 0, ny= 0, d=sd.GameData[43], },
+      { x = 0, y = 0, nx= 0, ny= 0, d=sd.GameData[44], },
+      { x = 0, y = 0, nx= 0, ny= 0, d=sd.GameData[45], },
+      { x = 0, y = 0, nx= 0, ny= 0, d=sd.GameData[46], },
+      { x = 0, y = 0, nx= 0, ny= 0, d=sd.GameData[47], },
+      { x = 0, y = 0, nx= 0, ny= 0, d=sd.GameData[48], },
+      { x = 0, y = 0, nx= 0, ny= 0, d=sd.GameData[49], },
+      { x = 0, y = 0, nx= 0, ny= 0, d=sd.GameData[50], },
+      { x = 0, y = 0, nx= 0, ny= 0, d=sd.GameData[51], },
+      { x = 0, y = 0, nx= 0, ny= 0, d=sd.GameData[52], },
+      { x = 0, y = 0, nx= 0, ny= 0, d=sd.GameData[53], },
+      { x = 0, y = 0, nx= 0, ny= 0, d=sd.GameData[54], },
+      { x = 0, y = 0, nx= 0, ny= 0, d=sd.GameData[55], },
+      { x = 0, y = 0, nx= 0, ny= 0, d=sd.GameData[56], },
+      { x = 0, y = 0, nx= 0, ny= 0, d=sd.GameData[57], },
+      { x = 0, y = 0, nx= 0, ny= 0, d=sd.GameData[58], },
+      { x = 0, y = 0, nx= 0, ny= 0, d=sd.GameData[59], },
+      { x = 0, y = 0, nx= 0, ny= 0, d=sd.GameData[60], },
+      { x = 0, y = 0, nx= 0, ny= 0, d=sd.GameData[61], },
+      { x = 0, y = 0, nx= 0, ny= 0, d=sd.GameData[62], },
+      { x = 0, y = 0, nx= 0, ny= 0, d=sd.GameData[63], },
+      { x = 0, y = 0, nx= 0, ny= 0, d=sd.GameData[64], },
+      { x = 0, y = 0, nx= 0, ny= 0, d=sd.GameData[65], },
+      { x = 0, y = 0, nx= 0, ny= 0, d=sd.GameData[66], },
+      { x = 0, y = 0, nx= 0, ny= 0, d=sd.GameData[67], },
+      { x = 0, y = 0, nx= 0, ny= 0, d=sd.GameData[68], },
+      { x = 0, y = 0, nx= 0, ny= 0, d=sd.GameData[69], },
+      { x = 0, y = 0, nx= 0, ny= 0, d=sd.GameData[70], },
+      { x = 0, y = 0, nx= 0, ny= 0, d=sd.GameData[71], },
+      { x = 0, y = 0, nx= 0, ny= 0, d=sd.GameData[72], },
+      { x = 0, y = 0, nx= 0, ny= 0, d=sd.GameData[73], },
+      { x = 0, y = 0, nx= 0, ny= 0, d=sd.GameData[74], },
+      { x = 0, y = 0, nx= 0, ny= 0, d=sd.GameData[75], },
+      { x = 0, y = 0, nx= 0, ny= 0, d=sd.GameData[76], },
+      { x = 0, y = 0, nx= 0, ny= 0, d=sd.GameData[77], },
+      { x = 0, y = 0, nx= 0, ny= 0, d=sd.GameData[78], },
+      { x = 0, y = 0, nx= 0, ny= 0, d=sd.GameData[79], },
+      { x = 0, y = 0, nx= 0, ny= 0, d=sd.GameData[80], },
+      { x = 0, y = 0, nx= 0, ny= 0, d=sd.GameData[81], },
+   } -- >>>
+
+   Selected      = 1
+   ShowReminder  = true
+   IsComplete    = false
+   AutoFill      = false
+
+   updateData(Data, sd.GameData)
+   updateSelected()
+
+end -- >>>
+
+function love.keypressed(key, sc, isrepeat) -- <<<
+
+   if not IsComplete then
+
+      if (key == "left") or (key == "h") then
+         if Selected == 1 then
+            return
+         end
+         Selected = Selected - 1
+         updateSelected()
+         return
+      end
+
+      if (key == "right") or (key == "l")  then
+         if Selected == 81 then
+            return
+         end
+         Selected = Selected + 1
+         updateSelected()
+      end
+
+      if (key == "up") or (key == "k")  then
+         if (Selected >= 1) and (Selected <= 9) then
+            return
+         end
+         Selected = Selected - 9
+         updateSelected()
+         return
+      end
+
+      if (key == "down") or (key == "j")  then
+         if (Selected >= 73) and (Selected <= 81) then
+            return
+         end
+         Selected = Selected + 9
+         updateSelected()
+         return
+      end
+   end
+
+   if not isrepeat then
+
+      if key == "q" then
+         love.event.quit()
+      end
+
+      if not IsComplete then
+
+         if key == "r" then
+            ShowReminder = not ShowReminder
+         end
+
+         if key == "a" then
+            AutoFill = not AutoFill
+            if AutoFill then
+               while sd.autoFill() do end
+               IsComplete = sd.isComplete()
+            end
+         end
+
+         if key == "f" then
+            while sd.autoFill() do end
+            IsComplete = sd.isComplete()
+         end
+
+         if (key == "1") or (key == "2") or (key == "3") or (key == "4") or (key == "5") or (key == "6") 
+         or (key == "7") or (key == "8") or (key == "9")  or (key == "0")  or (key == "space") then
+            if sd.setValue(Selected, tonumber(key) or 0) then
+               if AutoFill then
+                  while sd.autoFill() do end
+               end
+               IsComplete = sd.isComplete()
+            end
+         end
+      end
+   end
+
+end -- >>>
+
+-- function love.mousepressed(x, y, button, istouch) -- <<<
+-- end -- >>>
+
+-- function love.mousereleased(x, y, button, istouch) -- <<<
+-- end -- >>>
+
+function love.resize(w, h) -- <<<
+   updateData(Data, sd.GameData)
+   updateSelected()
+end -- >>>
+
+function love.update(dt) -- <<<
+   -- love.timer.sleep(0.01)
+   -- love.window.setTitle(love.timer.getFPS())
+end -- >>>
+
+--function love.errhand(msg) -- <<<
+   --love.window.showMessageBox( "internal error", msg, "error", true )
+--end -- >>>
+
+function love.draw() -- <<<
+
+   local g = love.graphics
+
+   g.setBackgroundColor(1, 1, 1)
+
+   g.setLineWidth(1)
+   g.setColor(0,0,0)
+   for _,v in ipairs(Data) do
+      drawBox(v)
+   end
+
+   if not IsComplete then
+      g.setColor(0,0,0)
+   else
+      g.setColor(0,0,1)
+   end
+   g.setLineWidth(3)
+   g.rectangle("line", OffsetX, OffsetY, BoardSize, BoardSize)
+   g.line( XA      , OffsetY , XA , YC)
+   g.line( XB      , OffsetY , XB , YC)
+   g.line( OffsetX , YA      , XC , YA)
+   g.line( OffsetX , YB      , XC , YB)
+
+   if not IsComplete then
+      g.setColor(1,0,0)
+      g.rectangle("line", SelectedX, SelectedY, Size, Size)
+   end
+end -- >>>
+
+-- vim: fmr=<<<,>>> fdm=marker
---
+++ solvdoku/solvdoku.lua
@@ -0,0 +1,286 @@
+
+-- GameData <<<
+local GameData =
+{
+
+   {true,true,true,true,true,true,true,true,true,value_n=0,value_s='',row=1,col=1,fld=1},
+   {true,true,true,true,true,true,true,true,true,value_n=0,value_s='',row=1,col=2,fld=1},
+   {true,true,true,true,true,true,true,true,true,value_n=0,value_s='',row=1,col=3,fld=1},
+   {true,true,true,true,true,true,true,true,true,value_n=0,value_s='',row=1,col=4,fld=2},
+   {true,true,true,true,true,true,true,true,true,value_n=0,value_s='',row=1,col=5,fld=2},
+   {true,true,true,true,true,true,true,true,true,value_n=0,value_s='',row=1,col=6,fld=2},
+   {true,true,true,true,true,true,true,true,true,value_n=0,value_s='',row=1,col=7,fld=3},
+   {true,true,true,true,true,true,true,true,true,value_n=0,value_s='',row=1,col=8,fld=3},
+   {true,true,true,true,true,true,true,true,true,value_n=0,value_s='',row=1,col=9,fld=3},
+   {true,true,true,true,true,true,true,true,true,value_n=0,value_s='',row=2,col=1,fld=1},
+   {true,true,true,true,true,true,true,true,true,value_n=0,value_s='',row=2,col=2,fld=1},
+   {true,true,true,true,true,true,true,true,true,value_n=0,value_s='',row=2,col=3,fld=1},
+   {true,true,true,true,true,true,true,true,true,value_n=0,value_s='',row=2,col=4,fld=2},
+   {true,true,true,true,true,true,true,true,true,value_n=0,value_s='',row=2,col=5,fld=2},
+   {true,true,true,true,true,true,true,true,true,value_n=0,value_s='',row=2,col=6,fld=2},
+   {true,true,true,true,true,true,true,true,true,value_n=0,value_s='',row=2,col=7,fld=3},
+   {true,true,true,true,true,true,true,true,true,value_n=0,value_s='',row=2,col=8,fld=3},
+   {true,true,true,true,true,true,true,true,true,value_n=0,value_s='',row=2,col=9,fld=3},
+   {true,true,true,true,true,true,true,true,true,value_n=0,value_s='',row=3,col=1,fld=1},
+   {true,true,true,true,true,true,true,true,true,value_n=0,value_s='',row=3,col=2,fld=1},
+   {true,true,true,true,true,true,true,true,true,value_n=0,value_s='',row=3,col=3,fld=1},
+   {true,true,true,true,true,true,true,true,true,value_n=0,value_s='',row=3,col=4,fld=2},
+   {true,true,true,true,true,true,true,true,true,value_n=0,value_s='',row=3,col=5,fld=2},
+   {true,true,true,true,true,true,true,true,true,value_n=0,value_s='',row=3,col=6,fld=2},
+   {true,true,true,true,true,true,true,true,true,value_n=0,value_s='',row=3,col=7,fld=3},
+   {true,true,true,true,true,true,true,true,true,value_n=0,value_s='',row=3,col=8,fld=3},
+   {true,true,true,true,true,true,true,true,true,value_n=0,value_s='',row=3,col=9,fld=3},
+   {true,true,true,true,true,true,true,true,true,value_n=0,value_s='',row=4,col=1,fld=4},
+   {true,true,true,true,true,true,true,true,true,value_n=0,value_s='',row=4,col=2,fld=4},
+   {true,true,true,true,true,true,true,true,true,value_n=0,value_s='',row=4,col=3,fld=4},
+   {true,true,true,true,true,true,true,true,true,value_n=0,value_s='',row=4,col=4,fld=5},
+   {true,true,true,true,true,true,true,true,true,value_n=0,value_s='',row=4,col=5,fld=5},
+   {true,true,true,true,true,true,true,true,true,value_n=0,value_s='',row=4,col=6,fld=5},
+   {true,true,true,true,true,true,true,true,true,value_n=0,value_s='',row=4,col=7,fld=6},
+   {true,true,true,true,true,true,true,true,true,value_n=0,value_s='',row=4,col=8,fld=6},
+   {true,true,true,true,true,true,true,true,true,value_n=0,value_s='',row=4,col=9,fld=6},
+   {true,true,true,true,true,true,true,true,true,value_n=0,value_s='',row=5,col=1,fld=4},
+   {true,true,true,true,true,true,true,true,true,value_n=0,value_s='',row=5,col=2,fld=4},
+   {true,true,true,true,true,true,true,true,true,value_n=0,value_s='',row=5,col=3,fld=4},
+   {true,true,true,true,true,true,true,true,true,value_n=0,value_s='',row=5,col=4,fld=5},
+   {true,true,true,true,true,true,true,true,true,value_n=0,value_s='',row=5,col=5,fld=5},
+   {true,true,true,true,true,true,true,true,true,value_n=0,value_s='',row=5,col=6,fld=5},
+   {true,true,true,true,true,true,true,true,true,value_n=0,value_s='',row=5,col=7,fld=6},
+   {true,true,true,true,true,true,true,true,true,value_n=0,value_s='',row=5,col=8,fld=6},
+   {true,true,true,true,true,true,true,true,true,value_n=0,value_s='',row=5,col=9,fld=6},
+   {true,true,true,true,true,true,true,true,true,value_n=0,value_s='',row=6,col=1,fld=4},
+   {true,true,true,true,true,true,true,true,true,value_n=0,value_s='',row=6,col=2,fld=4},
+   {true,true,true,true,true,true,true,true,true,value_n=0,value_s='',row=6,col=3,fld=4},
+   {true,true,true,true,true,true,true,true,true,value_n=0,value_s='',row=6,col=4,fld=5},
+   {true,true,true,true,true,true,true,true,true,value_n=0,value_s='',row=6,col=5,fld=5},
+   {true,true,true,true,true,true,true,true,true,value_n=0,value_s='',row=6,col=6,fld=5},
+   {true,true,true,true,true,true,true,true,true,value_n=0,value_s='',row=6,col=7,fld=6},
+   {true,true,true,true,true,true,true,true,true,value_n=0,value_s='',row=6,col=8,fld=6},
+   {true,true,true,true,true,true,true,true,true,value_n=0,value_s='',row=6,col=9,fld=6},
+   {true,true,true,true,true,true,true,true,true,value_n=0,value_s='',row=7,col=1,fld=7},
+   {true,true,true,true,true,true,true,true,true,value_n=0,value_s='',row=7,col=2,fld=7},
+   {true,true,true,true,true,true,true,true,true,value_n=0,value_s='',row=7,col=3,fld=7},
+   {true,true,true,true,true,true,true,true,true,value_n=0,value_s='',row=7,col=4,fld=8},
+   {true,true,true,true,true,true,true,true,true,value_n=0,value_s='',row=7,col=5,fld=8},
+   {true,true,true,true,true,true,true,true,true,value_n=0,value_s='',row=7,col=6,fld=8},
+   {true,true,true,true,true,true,true,true,true,value_n=0,value_s='',row=7,col=7,fld=9},
+   {true,true,true,true,true,true,true,true,true,value_n=0,value_s='',row=7,col=8,fld=9},
+   {true,true,true,true,true,true,true,true,true,value_n=0,value_s='',row=7,col=9,fld=9},
+   {true,true,true,true,true,true,true,true,true,value_n=0,value_s='',row=8,col=1,fld=7},
+   {true,true,true,true,true,true,true,true,true,value_n=0,value_s='',row=8,col=2,fld=7},
+   {true,true,true,true,true,true,true,true,true,value_n=0,value_s='',row=8,col=3,fld=7},
+   {true,true,true,true,true,true,true,true,true,value_n=0,value_s='',row=8,col=4,fld=8},
+   {true,true,true,true,true,true,true,true,true,value_n=0,value_s='',row=8,col=5,fld=8},
+   {true,true,true,true,true,true,true,true,true,value_n=0,value_s='',row=8,col=6,fld=8},
+   {true,true,true,true,true,true,true,true,true,value_n=0,value_s='',row=8,col=7,fld=9},
+   {true,true,true,true,true,true,true,true,true,value_n=0,value_s='',row=8,col=8,fld=9},
+   {true,true,true,true,true,true,true,true,true,value_n=0,value_s='',row=8,col=9,fld=9},
+   {true,true,true,true,true,true,true,true,true,value_n=0,value_s='',row=9,col=1,fld=7},
+   {true,true,true,true,true,true,true,true,true,value_n=0,value_s='',row=9,col=2,fld=7},
+   {true,true,true,true,true,true,true,true,true,value_n=0,value_s='',row=9,col=3,fld=7},
+   {true,true,true,true,true,true,true,true,true,value_n=0,value_s='',row=9,col=4,fld=8},
+   {true,true,true,true,true,true,true,true,true,value_n=0,value_s='',row=9,col=5,fld=8},
+   {true,true,true,true,true,true,true,true,true,value_n=0,value_s='',row=9,col=6,fld=8},
+   {true,true,true,true,true,true,true,true,true,value_n=0,value_s='',row=9,col=7,fld=9},
+   {true,true,true,true,true,true,true,true,true,value_n=0,value_s='',row=9,col=8,fld=9},
+   {true,true,true,true,true,true,true,true,true,value_n=0,value_s='',row=9,col=9,fld=9},
+
+} -- >>>
+
+-- Rows <<<
+local Rows=
+{
+   { 1 ,  2 ,  3 ,  4 ,  5 ,  6 ,  7 ,  8 ,  9} , 
+   {10 , 11 , 12 , 13 , 14 , 15 , 16 , 17 , 18} , 
+   {19 , 20 , 21 , 22 , 23 , 24 , 25 , 26 , 27} , 
+   {28 , 29 , 30 , 31 , 32 , 33 , 34 , 35 , 36} , 
+   {37 , 38 , 39 , 40 , 41 , 42 , 43 , 44 , 45} , 
+   {46 , 47 , 48 , 49 , 50 , 51 , 52 , 53 , 54} , 
+   {55 , 56 , 57 , 58 , 59 , 60 , 61 , 62 , 63} , 
+   {64 , 65 , 66 , 67 , 68 , 69 , 70 , 71 , 72} , 
+   {73 , 74 , 75 , 76 , 77 , 78 , 79 , 80 , 81} , 
+} -- >>>
+
+-- Columns <<<
+local Columns=
+{
+   { 1 , 10 , 19 , 28 , 37 , 46 , 55 , 64 , 73} , 
+   { 2 , 11 , 20 , 29 , 38 , 47 , 56 , 65 , 74} , 
+   { 3 , 12 , 21 , 30 , 39 , 48 , 57 , 66 , 75} , 
+   { 4 , 13 , 22 , 31 , 40 , 49 , 58 , 67 , 76} , 
+   { 5 , 14 , 23 , 32 , 41 , 50 , 59 , 68 , 77} , 
+   { 6 , 15 , 24 , 33 , 42 , 51 , 60 , 69 , 78} , 
+   { 7 , 16 , 25 , 34 , 43 , 52 , 61 , 70 , 79} , 
+   { 8 , 17 , 26 , 35 , 44 , 53 , 62 , 71 , 80} , 
+   { 9 , 18 , 27 , 36 , 45 , 54 , 63 , 72 , 81} , 
+} -- >>>
+
+-- Fields <<<
+local Fields=
+{
+   {1  , 2  , 3  , 10 , 11 , 12 , 19 , 20 , 21}, 
+   {4  , 5  , 6  , 13 , 14 , 15 , 22 , 23 , 24}, 
+   {7  , 8  , 9  , 16 , 17 , 18 , 25 , 26 , 27}, 
+   {28 , 29 , 30 , 37 , 38 , 39 , 46 , 47 , 48}, 
+   {31 , 32 , 33 , 40 , 41 , 42 , 49 , 50 , 51}, 
+   {34 , 35 , 36 , 43 , 44 , 45 , 52 , 53 , 54}, 
+   {55 , 56 , 57 , 64 , 65 , 66 , 73 , 74 , 75}, 
+   {58 , 59 , 60 , 67 , 68 , 69 , 76 , 77 , 78}, 
+   {61 , 62 , 63 , 70 , 71 , 72 , 79 , 80 , 81}, 
+} -- >>>
+
+function helpUnset(i,v) -- <<<
+
+   if GameData[i].value_n ~= 0 then
+      goto continue
+   end
+
+   for _,x in ipairs(Rows[GameData[i].row]) do
+      if GameData[x].value_n == v then
+         goto continue
+      end
+   end
+
+   for _,x in ipairs(Columns[GameData[i].col]) do
+      if GameData[x].value_n == v then
+         goto continue
+      end
+   end
+
+   for _,x in ipairs(Fields[GameData[i].fld]) do
+      if GameData[x].value_n == v then
+         goto continue
+      end
+   end
+
+   GameData[i][v] = true
+
+   ::continue::
+end -- >>>
+
+function unsetValue(i) -- <<<
+
+   local ValueToUnset = GameData[i].value_n
+
+   GameData[i].value_n = 0
+   GameData[i].value_s = ''
+
+   for idx,_ in ipairs(GameData) do
+      helpUnset(idx,ValueToUnset)
+   end
+
+   for n=1,9 do
+      helpUnset(i,n)
+   end
+
+   return ValueToUnset
+end -- >>>
+
+function helpSet(i,v) -- <<<
+   -- check if value can be set
+   if GameData[i][v] == false then
+      return false
+   end
+
+   -- set value
+   for x,_ in ipairs(GameData[i]) do
+      GameData[i][x] = false
+   end
+   GameData[i].value_n = v
+   GameData[i].value_s = tostring(v)
+
+   -- set the correct reminder flags to false
+   for _,x in ipairs(Rows[GameData[i].row]) do
+      GameData[x][v] = false
+   end
+
+   for _,x in ipairs(Columns[GameData[i].col]) do
+      GameData[x][v] = false
+   end
+
+   for _,x in ipairs(Fields[GameData[i].fld]) do
+      GameData[x][v] = false
+   end
+
+   return true
+end -- >>>
+
+function setValue(i,v) -- <<<
+   -- 1.     0, not set -> do nothting -> return false
+   -- 2.     0,     set -> undo setting -> return true/false?
+   -- 3. not 0, not set -> set value -> return true
+   -- 4. not 0,     set -> undo setting and set value -> return true
+
+   if v == 0 then
+      if GameData[i].value_n == 0 then
+         return false
+      else
+         unsetValue(i)
+         return false
+         -- return true --> would trigger a completed-check
+      end
+   else
+      if GameData[i].value_n == 0 then
+         return helpSet(i,v)
+      else
+         local ValueUnset = unsetValue(i)
+         local SetSuccess = helpSet(i,v)
+         if SetSuccess then
+            return true
+         else
+            return helpSet(i,ValueUnset)
+         end
+      end
+   end
+end -- >>>
+
+function isComplete() -- <<<
+   for i,v in ipairs(GameData) do
+      for j,w in ipairs(GameData[i]) do
+         if GameData[i][j] then
+            return false
+         end
+      end
+   end
+   return true
+end -- >>>
+
+function getLastPossibleValue(i) -- <<<
+
+   local Counter = 0
+   local Value
+
+   for j,v in ipairs(GameData[i]) do
+      if v then
+         Counter = Counter + 1
+         Value   = j
+      end
+   end
+
+   if Counter == 1 then
+      return Value
+   else
+      return nil
+   end
+end -- >>>
+
+function autoFill() -- <<<
+   for i,v in ipairs(GameData) do
+      local Value = getLastPossibleValue(i)
+      if Value then
+         setValue(i, Value)
+         return true
+      end
+   end
+   return false
+end -- >>>
+
+return
+{
+   GameData   = GameData,
+   setValue   = setValue,
+   isComplete = isComplete,
+   autoFill   = autoFill,
+}
+
+-- vim: fmr=<<<,>>> fdm=marker
